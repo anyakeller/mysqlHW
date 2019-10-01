d430fedba830218e04a1dd6f2d7dbd4962ae919d
Anya L Keller <kelleranyal@gmail.com>
2019-10-01 19:12:27 -0400

    finished the hw

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,8 +20,68 @@ connection.connect(function(err) {
 
 connection.query("SELECT * FROM products", function(error, results, fields) {
     if (error) throw error;
-    console.log(results);
+    console.log("Products");
+    console.log("--------");
+    var products = [];
+    for (var i = 0; i < results.length; i++) {
+        products.push(results[i]);
+        displayItem(results[i]);
+    }
     // console.log(fields);
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "productid",
+                message: "Enter a product ID:"
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "Enter a quantity:"
+            }
+        ])
+        .then(function(ans) {
+            //check for in stock
+            var theItem = results[ans.productid - 1];
+            if (!theItem) {
+                console.log("Does not exisit!");
+                connection.end();
+            } else {
+                var resultQuantity = theItem.stock_quantity - ans.quantity;
+                if (resultQuantity >= 0) {
+                    updateStock(ans.productid, resultQuantity);
+                    console.log(
+                        "You bought " +
+                            ans.quantity +
+                            " of " +
+                            theItem.product_name
+                    );
+                } else {
+                    console.log("Insufficient Quantity");
+                    connection.end();
+                }
+            }
+        });
 });
 
-connection.end();
+function displayItem(item) {
+    console.log("Product:", item.product_name);
+    console.log("ID:", item.item_id);
+    console.log("Price:", item.price);
+    console.log("--------------");
+}
+
+function updateStock(id, newQuantity) {
+    connection.query(
+        "UPDATE products SET stock_quantity = " +
+            newQuantity +
+            " WHERE item_id = " +
+            id +
+            ";",
+        function(error, results, fields) {
+            if (error) throw error;
+        }
+    );
+    connection.end();
+}
